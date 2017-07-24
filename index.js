34b13a4abca8125adf5d01bb1c7547764a307361
Trey Shugart <treshugart@gmail.com>
2017-07-24 14:01:07 +1000

    Deduped rehydration function and allow for a custom name option.
    BREAKING CHANGE: second param to render() is now an object. Specify the old arg (resolver) as "resolver" now.

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-function stringify (node) {
+function ssrScript (funcName) {
+  return `<script>function ${funcName}(){var a=document.currentScript.previousElementSibling,b=a.firstElementChild;a.removeChild(b);for(var c=a.attachShadow({mode:"open"});b.hasChildNodes();)c.appendChild(b.firstChild);}</script>`;
+}
+
+function stringify (node, { funcName }) {
   const { attributes = [], childNodes, nodeName, nodeValue, shadowRoot } = node;
   if (nodeName === '#text') {
     return nodeValue;
   }
   const attrsAsString = Array.from(attributes || []).map(({ name, value }) => ` ${name}="${value}"`);
   const localName = nodeName.toLowerCase();
-  const shadowNodes = shadowRoot ? stringify(shadowRoot) : '';
-  const lightNodes = childNodes.map(stringify).join('');
-  const rehydrationScript = shadowRoot ? `<script>var a=document.currentScript.previousElementSibling,b=a.firstElementChild;a.removeChild(b);for(var c=a.attachShadow({mode:"open"});b.hasChildNodes();)c.appendChild(b.firstChild);</script>` : '';
+  const shadowNodes = shadowRoot ? stringify(shadowRoot, { funcName }) : '';
+  const lightNodes = childNodes.map(stringify, { funcName }).join('');
+  const rehydrationScript = shadowRoot ? `<script>${funcName}()</script>` : '';
   return `<${localName}${attrsAsString}>${shadowNodes}${lightNodes}</${localName}>${rehydrationScript}`;
 }
 
-function render (node, resolver = setTimeout) {
+function render (node, opts) {
+  const { funcName, resolver } = Object.assign({}, {
+    funcName: '__ssr',
+    resolver: setTimeout
+  }, opts);
   document.body.appendChild(node);
   return new Promise(resolve => {
     resolver(() => {
-      resolve(stringify(node));
+      resolve(ssrScript(funcName) + stringify(node, { funcName }));
       document.body.removeChild(node);
     });
   });
